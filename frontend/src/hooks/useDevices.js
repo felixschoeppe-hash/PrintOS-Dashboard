a50e17d90a6b520aa11280d7cad5adf1657fffab
felixschoeppe-hash <felix.schoeppe@googlemail.com>
2025-12-30 22:28:05 +0100

    Fix syntax error in useDevices hook

--- frontend/src/hooks/useDevices.js
+++ frontend/src/hooks/useDevices.js
@@ -1,4 +1,4 @@
-﻿import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const API_URL = 'https://printos-backend.onrender.com/api';
@@ -15,17 +15,19 @@ export const useDevices = () => {
   const fetchDevices = async () => {
     try {
       const token = localStorage.getItem('token');
-      const response = await axios.get(\\/settings/devices\, {
-        headers: { Authorization: \Bearer \\ }
+      const response = await axios.get(API_URL + '/settings/devices', {
+        headers: { Authorization: 'Bearer ' + token }
       });
 
       setDevices(response.data.devices || []);
       
       // Erstelle deviceNames Object: { "47200413": "Custom Name", ... }
       const names = {};
-      response.data.devices?.forEach(device => {
-        names[device.device_id] = device.name || device.device_id;
-      });
+      if (response.data.devices) {
+        response.data.devices.forEach(device => {
+          names[device.device_id] = device.name || device.device_id;
+        });
+      }
       names['all'] = 'Alle Pressen';
       
       setDeviceNames(names);
